3bbadd41af436874cd7d5af8f1a017f8e8546d13
netotz <neto.otz@hotmail.com>
2022-03-08 09:57:40 -0600

    Add dynamic component

---
+++ WiredBrainCoffee.UI/Pages/ContactForm.razor.cs
@@ -0,0 +1,24 @@
+﻿using WiredBrainCoffee.UI.Components;
+
+namespace WiredBrainCoffee.UI.Pages;
+
+public partial class ContactForm
+{
+    public Type ContactType { get; private set; }
+
+    protected override void OnInitialized()
+    {
+        ContactType = typeof(CustomerContact);
+        base.OnInitialized();
+    }
+
+    private void OnSelectedContactTypeChanged(string newContactType)
+    {
+        ContactType = newContactType switch
+        {
+            nameof(CustomerContact) => typeof(CustomerContact),
+            nameof(PartnerContact) => typeof(PartnerContact),
+            _ => throw new ArgumentException($"The type {newContactType} is not valid."),
+        };
+    }
+}

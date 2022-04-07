4cc482ae79e4b14799dca7eb401cbb6d3f635d2f
netotz <neto.otz@hotmail.com>
2022-04-07 15:18:17 -0500

    update packages

--- WiredBrainCoffee.UI/Program.cs
+++ WiredBrainCoffee.UI/Program.cs
@@ -19,12 +19,9 @@ builder.RootComponents.RegisterForJavaScript<GlobalAlert>(identifier: "globalAle
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazoredModal();
-builder.Services.AddBlazorise(options =>
-{
-    options.ChangeTextOnKeyPress = true;
-})
-  .AddBootstrapProviders()
-  .AddFontAwesomeIcons();
+builder.Services.AddBlazorise()
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 builder.Services.AddHttpClient<IMenuService, MenuService>(client =>
     client.BaseAddress = new Uri("https://localhost:7024/"));
